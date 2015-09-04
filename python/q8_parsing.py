79f6e1acc274cb6040b01bcf1405ad7a30d6625a
John-Keating <data.keating@gmail.com>
2015-09-04 11:19:45 -0400

    created basic function, no ties

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -1,12 +1,20 @@
-#The football.csv file contains the results from the English Premier League.
-# The columns labeled ‘Goals’ and ‘Goals Allowed’ contain the total number of
+# The football.csv file contains the results from the English Premier League.
+# The columns labeled Goals and Goals Allowed contain the total number of
 # goals scored for and against each team in that season (so Arsenal scored 79 goals
 # against opponents, and had 36 goals scored against them). Write a program to read the file,
-# then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
+# then print the name of the team with the smallest difference in for and against goals.
 
 
 import csv
 
-def read_data(data):
+
+def min_team(data):
     team_results = csv.DictReader(open(data, 'rb'))
-    return team_results
+    mindiff = None
+    team_name = None
+    for line in team_results:
+        diff = abs(int(line['Goals']) - int(line['Goals Allowed']))
+        if mindiff == None or mindiff < diff:
+            mindiff = diff
+            team_name = line['Team']
+    return team_name
