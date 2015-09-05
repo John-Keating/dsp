5c90352adc659c1a5d6925d82a6e0a8018474a27
John-Keating <data.keating@gmail.com>
2015-09-05 05:19:55 -0400

    added d.get() histogram function

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -2,20 +2,21 @@
 
 
 import pandas as pd  # import pandas library
-import string
 
 
 def read_data(data):
-    facultydf = pd.read_csv(data)
+    facultydf = pd.read_csv(data, na_values=[""])
     return facultydf
 
 
 def number_freq(data, request):
-    req_list = []
-    req_dict = {}
+    req_l = []
+    req_d = {}
     req_raw = data[request]
     for row in req_raw:
         no_punc = row.replace(".", "")
-        req_list.append(no_punc.split())
-    flat_list = [item for sublist in req_list
-    return req_list
+        req_l.append(no_punc.split())
+    flat_list = [item for sublist in req_l for item in sublist]
+    for item in flat_list:
+        req_d[item] = 1 + req_d.get(item, 0)
+    return req_d
