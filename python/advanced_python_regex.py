5f993225637baa51a1ad622ef333430b1b59bc35
John-Keating <data.keating@gmail.com>
2015-09-05 06:16:38 -0400

    Q3 and 4, emails, 1st

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -6,10 +6,11 @@ import pandas as pd  # import pandas library
 #  read_data reads a cvs file and puts it into a pandas dataframe
 #  empty entries are recorded as na values
 
+
 def read_data(data):
     facultydf = pd.read_csv(data, na_values=[""])
     return facultydf
-
+# I should add a line above that clears whitespace from the col names
 
 #  reads data into data frame, selects information you request
 #  Cleans selection and returns dictrionary histogram of data
@@ -28,15 +29,28 @@ def number_freq(data, request):
         req_d[item] = 1 + req_d.get(item, 0)
     return req_d
 
+
 def num_titles(data):
     df = read_data(data)
     title_d = {}
     titles_raw = df['email'].tolist()
     for item in titles_raw:
         title = item.partition("of")[0]
-        title_d[item] = 1 + title_d.get(item, 0)
+        title_d[title] = 1 + title_d.get(title, 0)
     return title_d
 
 
 def all_email(data):
-    email
+    df = read_data(data)
+    email_list = df['email'].tolist()
+    return email_list
+
+
+def all_domains(data):
+    emails = all_email(data)
+    domains = []
+    for item in emails:
+        dom = item.partition("@")[2]
+        domains.append(dom)
+    uniq_domains = set(domains)
+    return uniq_domains
