f80017f56b35b0b8567e5caa2d1717bf3778246c
John-Keating <data.keating@gmail.com>
2015-09-05 06:03:56 -0400

    titles histogram first draft, first attempt

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -3,16 +3,23 @@
 
 import pandas as pd  # import pandas library
 
+#  read_data reads a cvs file and puts it into a pandas dataframe
+#  empty entries are recorded as na values
 
 def read_data(data):
     facultydf = pd.read_csv(data, na_values=[""])
     return facultydf
 
 
+#  reads data into data frame, selects information you request
+#  Cleans selection and returns dictrionary histogram of data
+
+
 def number_freq(data, request):
+    df = read_data(data)
     req_l = []
     req_d = {}
-    req_raw = data[request]
+    req_raw = df[request]
     for row in req_raw:
         no_punc = row.replace(".", "")
         req_l.append(no_punc.split())
@@ -20,3 +27,16 @@ def number_freq(data, request):
     for item in flat_list:
         req_d[item] = 1 + req_d.get(item, 0)
     return req_d
+
+def num_titles(data):
+    df = read_data(data)
+    title_d = {}
+    titles_raw = df['email'].tolist()
+    for item in titles_raw:
+        title = item.partition("of")[0]
+        title_d[item] = 1 + title_d.get(item, 0)
+    return title_d
+
+
+def all_email(data):
+    email
