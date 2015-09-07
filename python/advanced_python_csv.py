0c6f00983ea0ee3dc48770ff6f0f5f68dbab615f
John-Keating <data.keating@gmail.com>
2015-09-07 16:49:19 -0400

    read data function to the file

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,12 +1,17 @@
 # this function takes output from other functions and writes it as csv file
 
-import pandas
+import pandas as pd
 
+def read_data(data):
+    df = pd.read_csv(data, na_values=[""])
+    df.rename(columns= lambda x: x.strip(), inplace= True) #remove wspace
+    df.fillna(value="n/a", inplace=True)
+    return df
 
 def all_email(data):
     df = read_data(data)
     email_list = df['email'].tolist()
-    return set(email_list)
+    return email_list
 
 
 def produce_csv(output, name):
