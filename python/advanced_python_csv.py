8cf10fab8a98c7721a0ebe49665ab63f0e9320dd
John-Keating <data.keating@gmail.com>
2015-09-07 07:15:30 -0400

    added all_email function to file

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -3,5 +3,15 @@
 import pandas
 
 
+def all_email(data):
+    df = read_data(data)
+    email_list = df['email'].tolist()
+    return set(email_list)
+
+
 def produce_csv(output, name):
     output.to_csv(name)
+
+emails = all_email("faculty.csv")
+
+produce_csv(emails, "email.csv"
